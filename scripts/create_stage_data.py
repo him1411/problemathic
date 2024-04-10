953246bbf3dc010d81368899164894360e76089d
kscaria <scariakevin1@gmail.com>
2024-04-09 21:18:22 -0700

    Refactoring code for generalization

--- scripts/create_stage_data.py
+++ scripts/create_stage_data.py
@@ -2,12 +2,10 @@ import os
 import sys
 import json
 from dotenv import load_dotenv
+from langchain.chat_models import AzureChatOpenAI
 
 sys.path.append("./")
 from problemathic import AdversarialMathDataGen
-
-# AZURE ENDPOINT FLAG
-azure_api = True # when using gpt-4 chat keys
 
 # Load environment variables
 load_dotenv()
@@ -16,21 +14,15 @@ load_dotenv()
 HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-# OPENAI_API_BASE = os.getenv("OPENAI_API_BASE_AI23", os.getenv("OPENAI_API_BASE_AI23"))
-# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY_AI23", os.getenv("OPENAI_API_KEY_AI23"))
+
 root_path = "./"
-trial = True
-if azure_api:
-    model_keys = {
-        "openai_api_base": os.getenv("OPENAI_API_BASE_AI23", os.getenv("OPENAI_API_BASE_AI23")),
-        "api_key": os.getenv("OPENAI_API_KEY_AI23", os.getenv("OPENAI_API_KEY_AI23"))
-    }
+
 
 # Set Constants
 input_tuples = [
-    # ("./dataset/curated/stage1_train_processed_data.json", "./dataset/processed/json/stage_one_temp.json", 1),
-    # ("./dataset/curated/stage2_train_processed_data.json", "./dataset/processed/json/stage_two.json", 2),
-    ("./dataset/curated/stage3_train_processed_data.json", "./dataset/processed/json/stage_three.json", 3)
+    # ("./dataset/curated/stage1_train_processed_data.json", "./dataset/generated/json/stage_one_temp.json", 1),
+    # ("./dataset/curated/stage2_train_processed_data.json", "./dataset/generated/json/stage_two.json", 2),
+    ("./dataset/curated/stage3_train_processed_data.json", "./dataset/generated/json/stage_three.json", 3)
     ]
 
 start_idx=0
@@ -41,6 +33,21 @@ use_langchain_core=True
 get_explanation=True
 max_retries=3
 
+# Running with Azure EndPoint
+model_keys = {
+    "openai_api_base": os.getenv("OPENAI_API_BASE_AI23", os.getenv("OPENAI_API_BASE_AI23")),
+    "api_key": os.getenv("OPENAI_API_KEY_AI23", os.getenv("OPENAI_API_KEY_AI23"))
+}
+model = AzureChatOpenAI(
+                        deployment_name="gpt-4",
+                        openai_api_version="2023-03-15-preview",
+                        max_tokens=2048,
+                        temperature=0.3,
+                        max_retries=max_retries,
+                        request_timeout=5,
+                        **model_keys
+                    )
+
 for input_tuple in input_tuples:
     
     input_path, output_path, stage_type = input_tuple
@@ -48,13 +55,11 @@ for input_tuple in input_tuples:
     with open(input_path) as f:
         docs = json.load(f)
 
-    madgen = AdversarialMathDataGen(model="openai_gpt4_chat",
+    madgen = AdversarialMathDataGen(model=model,
                                     api_key=OPENAI_API_KEY, 
                                     use_langchain_core=use_langchain_core,
                                     max_retries=max_retries,
-                                    debug=debug,
-                                    keys = model_keys,
-                                    azure_api = azure_api)
+                                    debug=debug)
     madgen.transform(docs=docs,
                      stage_type=stage_type,
                      start_idx=start_idx,
